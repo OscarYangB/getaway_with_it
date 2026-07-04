b026d60a789b40277b2782eaf51dc972dad5d176
oscaryangb <oscaryangb@gmail.com>
2026-07-04 11:03:38 -0400

    Sync scene changes

--- Code/CameraState.cs
+++ Code/CameraState.cs
@@ -16,6 +16,7 @@ public partial class CameraState : Resource
 	[Export] public Dictionary<CameraDirection, int> transitions;
 	[Export] public float blurFarDistance = 8000.0f;
 	[Export] public float blurNearDistance = 0.1f;
+	[Export] public bool isDialogVisible = false;
 
 	public CameraState()
 	{
