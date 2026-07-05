b0ae09bf1385b364b9bf078a3f6177673db9cd49
oscaryangb <oscaryangb@gmail.com>
2026-07-05 10:41:42 -0400

    Functional screen UI

---
+++ Code/MoneyController.cs
@@ -0,0 +1,47 @@
+using Godot;
+using System;
+
+public partial class MoneyController : Node
+{
+	[Export] Texture2D offButtonTexture1;
+	[Export] Texture2D onButtonTexture1;
+	[Export] Texture2D offButtonTexture2;
+	[Export] Texture2D onButtonTexture2;
+	[Export] Texture2D offButtonTexture3;
+	[Export] Texture2D onButtonTexture3;
+
+	[Export] NodePath button1;
+	[Export] NodePath button2;
+	[Export] NodePath button3;
+
+	int moneyIndex = 0;
+
+	public override void _Ready()
+	{
+		GetNode<TextureButton>(button1).Pressed += () => ButtonPressed(1);
+		GetNode<TextureButton>(button2).Pressed += () => ButtonPressed(2);
+		GetNode<TextureButton>(button3).Pressed += () => ButtonPressed(3);
+	}
+
+	Texture2D getButtonTexture(int index, bool on)
+	{
+		if (index == 1) return on ? onButtonTexture1 : offButtonTexture1;
+		if (index == 2) return on ? onButtonTexture2 : offButtonTexture2;
+		if (index == 3) return on ? onButtonTexture3 : offButtonTexture3;
+		return null;
+	}
+
+	void ButtonPressed(int index)
+	{
+		moneyIndex = index;
+		GetNode<TextureButton>(button1).TextureNormal = getButtonTexture(1, index == 1);
+		GetNode<TextureButton>(button2).TextureNormal = getButtonTexture(2, index == 2);
+		GetNode<TextureButton>(button3).TextureNormal = getButtonTexture(3, index == 3);
+	}
+
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
---
+++ Code/PeopleController.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+using System.Text.RegularExpressions;
+
+public partial class PeopleController : Node
+{
+	int count = 0;
+	[Export] NodePath upButton;
+	[Export] NodePath downButton;
+	[Export] NodePath label;
+
+	public override void _Ready()
+	{
+		GetNode<TextureButton>(upButton).Pressed += goUp;
+		GetNode<TextureButton>(downButton).Pressed += goDown;
+		refreshLabel();
+	}
+
+	void refreshLabel()
+	{
+		count = Math.Clamp(count, 0, 20);
+		GetNode<Label>(label).Text = count.ToString();
+	}
+
+	void goUp()
+	{
+		count++;
+		refreshLabel();
+	}
+
+	void goDown()
+	{
+		count--;
+		refreshLabel();
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
